5dd0587b785a0bbed6897f8b6527dd1afa3f304c
Anthony Shivakumar <anthony.shivakumar@antshiv.com>
2025-01-27 06:12:04 -0800

    Added macro for deg2dar and viceversa

--- include/attitude/attitude_utils.h
+++ include/attitude/attitude_utils.h
@@ -12,6 +12,10 @@ extern "C" {
  * - unit conversions (degrees <-> radians)
  */
 
+// Macros for degree-radian conversion
+#define DEG2RAD(x) ((x) * M_PI / 180.0)
+#define RAD2DEG(x) ((x) * 180.0 / M_PI)
+
 double deg2rad(double degrees);
 double rad2deg(double radians);
 double wrap_angle(double angle);
